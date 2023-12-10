d330eac3e80afba022669064147b8e439f859ced
AshwiniVHM <153367015+AshwiniVHM@users.noreply.github.com>
2023-12-11 06:26:58 +1100

    Update data.sql

--- user-demo/src/main/resources/data.sql
+++ user-demo/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into user_details(phone_number, id,email_id,name) values(123,1, 'srikanthvhm@gmail.com','Srikanth');
-insert into user_details(phone_number, id,email_id,name) values(1234,2, 'Ashwini.hiremath@gmail.com','Ashwini');
-insert into user_details(phone_number, id,email_id,name) values(321,4, 'ashri@gmail.com','Ashri');
-insert into user_details(phone_number, id,email_id,name) values(124,3, 'jadesh@gmail.com','Jadesh');
+insert into user_details(phone_number, id,email_id,name) values('146623',1L, 'srikanthvhm@gmail.com','Srikanth');
+insert into user_details(phone_number, id,email_id,name) values('123489',2L, 'Ashwini.hiremath@gmail.com','Ashwini');
+insert into user_details(phone_number, id,email_id,name) values('32105',4L, 'ashri@gmail.com','Ashri');
+insert into user_details(phone_number, id,email_id,name) values('12456',3L, 'jadesh@gmail.com','Jadesh');
